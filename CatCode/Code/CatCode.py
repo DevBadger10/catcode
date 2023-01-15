02ac0440211d35344b5525dda7a2507449e405d0
DevBadger10 <104037142+DevBadger10@users.noreply.github.com>
2023-01-15 15:39:24 +1100

    Fixed #1
    WHY DID IT NEED A VARIABLE EQUIVELANT??? WHY??? WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY

--- CatCode/Code/CatCode.py
+++ CatCode/Code/CatCode.py
@@ -34,93 +34,99 @@ except:
 
 def number(input):
     output = 0
+
+    length = len(input) # Wait, so let me get this straight; passing range() len(input) leads to only one iteration, but feeding it a variable equivelant to len(input) works? *internal development screaming*
+    # print(str(length))
+
     # print(str(range(len(input))))
     # for n in range(len(input)):
     #     print(n)
     # print("123457890"[5])
-    for i in range(len(input)):
+    for i in range(0, length):
         if input[i] == "1":
             output += 1
         elif input[i]  ==  "2":
-            output += 2
+          output += 2
         elif input[i]  ==  "3":
-            output -= 3
+          output -= 3
         elif input[i]  ==  "4":
-            output -= 4
+          output -= 4
         elif input[i]  ==  "5":
-            output += 5
+          output += 5
         elif input[i]  ==  "6":
-            output += 6
+          output += 6
         elif input[i]  ==  "7":
-            output -= 7
+          output -= 7
         elif input[i]  ==  "8":
-            output -= 8
+          output -= 8
         elif input[i]  ==  "9":
-            output -= 9
+          output -= 9
         elif input[i]  ==  "0":
-            if str(output).startswith("-"):
-                output = int(str(output[1:]))
-            else:
-                output = int("-" + str(output))
+          if str(output).startswith("-"):
+              output = int(str(output[1:]))
+          else:
+              output = int("-" + str(output))
         elif input[i]  ==  "q":
-            output *= 10
+          output *= 10
         elif input[i]  ==  "w":
-            output *= 100
+          output *= 100
         elif input[i]  ==  "e":
-            output *= 1000
+          output *= 1000
         elif input[i]  ==  "r":
-            output -= 2
+          output -= 2
         elif input[i]  ==  "t":
-            output -= 2
+          output -= 2
         elif input[i]  ==  "y":
-            output -= 2
+          output -= 2
         elif input[i]  ==  "u":
-            if not input[i] == 0:
-                output /= 10
+          if not input[i] == 0:
+              output /= 10
         elif input[i]  ==  "i":
-            if not input[i] == 0:
-                output /= 100
+          if not input[i] == 0:
+              output /= 100
         elif input[i]  ==  "o":
-            if not input[i] == 0:
-                output /= 1000
+          if not input[i] == 0:
+              output /= 1000
         elif input[i]  ==  "p":
-            output -= 100
+          output -= 100
         elif input[i]  ==  "a":
-            output *= 3
+          output *= 3
         elif input[i]  ==  "s":
-            output *= 2
+          output *= 2
         elif input[i]  ==  "d":
-            output += 3
+          output += 3
         elif input[i]  ==  "f":
-            output += 2
+          output += 2
         elif input[i]  ==  "g":
-            output += 1
+          output += 1
         elif input[i]  ==  "h":
-            output -= 1
+          output -= 1
         elif input[i]  ==  "j":
-            output -= 2
+          output -= 2
         elif input[i]  ==  "k":
-            output -= 3
+          output -= 3
         elif input[i]  ==  "l":
-            if not input[i] == 0:
-                output /= 2
+          if not input[i] == 0:
+              output /= 2
         elif input[i]  ==  "z":
-            output *= 4
+          output *= 4
         elif input[i]  ==  "x":
-            if not input[i] == 0:
-                output /= 4
+          if not input[i] == 0:
+              output /= 4
         elif input[i]  ==  "c":
-            output += 2
+          output += 2
         elif input[i]  ==  "v":
-            output += 2
+          output += 2
         elif input[i]  ==  "b":
-            output += 2
+          output += 2
         elif input[i]  ==  "n":
-            output += 2
+          output += 2
         elif input[i]  ==  "m":
-            if not input[i] == 0:
-                output /= 3
-        return(str(output))
+          if not input[i] == 0:
+              output /= 3
+        # print(str(output))
+        # print(str(i))
+    return(str(output))
 
 def turtle(input):
     for i in len(input):
