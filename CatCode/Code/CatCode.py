8d3b8fcf88790310f39ef22b82bb194fd89b72b2
DevBadger10 <104037142+DevBadger10@users.noreply.github.com>
2023-01-19 12:45:29 +1100

    Y U NO WORK

--- CatCode/Code/CatCode.py
+++ CatCode/Code/CatCode.py
@@ -352,7 +352,7 @@ def game(input):
             gcol5 = colour(input[10] + input[11], False)
 
         if gtype == False:
-            if execPong(gspeed, gcol1, gcol2): noError = True
+            execPong(gspeed, gcol1, gcol2)
         if gtype == True:
             if execSnake(gspeed, gcol1, gcol2, gcol3, gcol4, gcol5): noError = True
     except:
@@ -365,15 +365,9 @@ import pygame
 from random import * # This is created by https://www.101computing.net/pong-tutorial-using-pygame-adding-a-scoring-system/ I simply brought it into one file and made the speed customizeable. 
 from random import randint
 import random
-pygame.init()
 speed = """ + str(speed) + """
-
 pygame.init()
-
 class Paddle(pygame.sprite.Sprite):
-    import pygame
-    from random import randint
-    import random
     #This class represents a paddle. It derives from the "Sprite" class in Pygame.
     def __init__(self, color, width, height):
         # Call the parent class (Sprite) constructor
@@ -383,7 +377,6 @@ class Paddle(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         self.image.fill(pygame.Color(""" + str(col1[0]) + """,""" + str(col1[1]) + """,""" + str(col1[2]) + """))
         self.image.set_colorkey(pygame.Color(""" + str(col1[0]) + """,""" + str(col1[1]) + """,""" + str(col1[2]) + """))
-
         # Draw the paddle (a rectangle!)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         # Fetch the rectangle object that has the dimensions of the image.
@@ -399,10 +392,6 @@ class Paddle(pygame.sprite.Sprite):
         if self.rect.y > 400:
           self.rect.y = 400
 class Ball(pygame.sprite.Sprite):
-    import pygame
-#    from random import * # Thi
-    from random import randint
-    import random
     #This class represents a ball. It derives from the "Sprite" class in Pygame.
     def __init__(self, color, width, height):
         from random import randint
@@ -413,7 +402,6 @@ class Ball(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         self.image.fill(pygame.Color(""" + str(col1[0]) + """,""" + str(col1[1]) + """,""" + str(col1[2]) + """))
         self.image.set_colorkey(pygame.Color(""" + str(col1[0]) + """,""" + str(col1[1]) + """,""" + str(col1[2]) + """))
-
         # Draw the ball (a rectangle!)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         self.velocity = [randint(4,8),randint(-8,8)]
@@ -427,53 +415,42 @@ class Ball(pygame.sprite.Sprite):
         from random import randint
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = randint(-8,8)
-
-
+pygame.init()
 # Open a new window
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
-
 paddleA = Paddle(pygame.Color(""" + str(col2[0]) + """,""" + str(col2[1]) + """,""" + str(col2[2]) + """), 10, 100)
 paddleA.rect.x = 20
 paddleA.rect.y = 200
-
 paddleB = Paddle(pygame.Color(""" + str(col2[0]) + """,""" + str(col2[1]) + """,""" + str(col2[2]) + """), 10, 100)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
-
 ball = Ball(pygame.Color(""" + str(col2[0]) + """,""" + str(col2[1]) + """,""" + str(col2[2]) + """),10,10)
 ball.rect.x = 345
 ball.rect.y = 195
-
 #This will be a list that will contain all the sprites we intend to use in our game.
 all_sprites_list = pygame.sprite.Group()
-
 # Add the 2 paddles and the ball to the list of objects
 all_sprites_list.add(paddleA)
 all_sprites_list.add(paddleB)
 all_sprites_list.add(ball)
-
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 carryOn = True
-
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
-
 #Initialise player scores
 scoreA = 0
 scoreB = 0
-
 # -------- Main Program Loop -----------
 while carryOn:
     # --- Main event loop
     for event in pygame.event.get(): # User did something
-        #if event.type == pygame.QUIT: # If user clicked close
-        #      carryOn = False # Flag that we are done so we exit this loop
-        if event.type==pygame.KEYDOWN:
+        if event.type == pygame.QUIT: # If user clicked close
+              carryOn = False # Flag that we are done so we exit this loop
+        elif event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_x: #Pressing the x Key will quit the game
                      carryOn=False
-
     #Moving the paddles when the use uses the arrow keys (player A) or "W/S" keys (player B) 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -484,7 +461,6 @@ while carryOn:
         paddleB.moveUp(speed)
     if keys[pygame.K_DOWN]:
         paddleB.moveDown(speed)    
-
     # --- Game logic should go here
     all_sprites_list.update()
     #Check if the ball is bouncing against any of the 4 walls:
@@ -498,7 +474,6 @@ while carryOn:
         ball.velocity[1] = -ball.velocity[1]
     if ball.rect.y<0:
         ball.velocity[1] = -ball.velocity[1]     
-
     #Detect collisions between the ball and the paddles
     if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
       ball.bounce()
@@ -509,23 +484,20 @@ while carryOn:
     pygame.draw.line(screen, pygame.Color(""" + str(col2[0]) + """,""" + str(col2[1]) + """,""" + str(col2[2]) + """), [349, 0], [349, 500], 5)
     #Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
     all_sprites_list.draw(screen) 
-
     #Display scores:
     font = pygame.font.Font(None, 74)
     text = font.render(str(scoreA), 1, pygame.Color(""" + str(col2[0]) + """,""" + str(col2[1]) + """,""" + str(col2[2]) + """))
     screen.blit(text, (250,10))
     text = font.render(str(scoreB), 1, pygame.Color(""" + str(col2[0]) + """,""" + str(col2[1]) + """,""" + str(col2[2]) + """))
     screen.blit(text, (420,10))
-
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
     # --- Limit to 60 frames per second
     clock.tick(60)
-
 #Once we have exited the main program loop we can stop the game engine:
 pygame.quit()"""
 
-    print(toExec)
+    # print(toExec)
     print("Game starting...")
     exec(toExec)
 
